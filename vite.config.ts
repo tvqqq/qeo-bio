9a215a0d186b5f23622690a5aa6f14b7c892b5ea
Quyen Tat <tvq9612@gmail.com>
2023-06-02 09:50:14 +0700

    feat: integrate page cv

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vercel()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        cv: resolve(__dirname, "cv/index.html"),
+      },
+    },
+  },
 });
